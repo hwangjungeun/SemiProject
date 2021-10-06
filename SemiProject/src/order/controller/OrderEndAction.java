0ca1f5557f8e0e8186e0498f1fe354500eb89665
﻿OhHyeonJi3088 <orange3088@gmail.com>
2021-10-06 16:13:57 +0900

    주석수정(별거아님-나중에 할거정리)

--- SemiProject/src/order/controller/OrderEndAction.java
+++ SemiProject/src/order/controller/OrderEndAction.java
@@ -13,7 +13,8 @@ public class OrderEndAction extends AbstractController {
 		// orderForm으로부터 get방식으로 가져온거(form태그의 name)
 		// destination,receiverName,postcode,address,detailAddress,extraAddress,hp1,hp2,hp3,email,deliveryMsg
 		
-		// 결제성공하면, 주문내역테이블에 넣어주고, 주문번호와 주문일자를 채번으로 가져와서 orderEnd.jsp에 넘겨줄꺼야~~
+		// 결제성공하면, 주문내역테이블에 넣어주고, 
+		// 주문번호와 주문일자를 채번으로 가져와서 orderEnd.jsp에 넘겨줄꺼야~~
 		
 //		super.setRedirect(false);
 		super.setViewPage("/WEB-INF/order/orderEnd.jsp");
