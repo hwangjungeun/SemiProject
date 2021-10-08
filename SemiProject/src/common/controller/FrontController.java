cb56d8d227c91c9e1fb62cb2d2c8300fe44de827
82108 <82108@DESKTOP-UD3V2Q5>
2021-10-08 16:24:34 +0900

    테스트 실패

--- SemiProject/src/common/controller/FrontController.java
+++ SemiProject/src/common/controller/FrontController.java
@@ -207,5 +207,5 @@ public class FrontController extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-	test
+	
 }
