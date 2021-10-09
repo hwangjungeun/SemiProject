a02dbc276b1e3f85d899dd30c723c79c3bbeeac8
﻿OhHyeonJi3088 <orange3088@gmail.com>
2021-10-09 21:50:49 +0900

    주문리스트 충돌 사전에 방지(역할분배 : 상품->회원)

--- SemiProject/src/mypage/controller/OrderListAction_OHJ.java
+++ SemiProject/src/mypage/controller/OrderListAction_OHJ.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
 
-public class OrderListAction extends AbstractController {
+public class OrderListAction_OHJ extends AbstractController {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
