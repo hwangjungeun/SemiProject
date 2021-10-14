d39a9cb63556318d578d88436471cc596e27ea86
kimmk <idtiktok@naver.com>
2021-10-14 17:16:34 +0900

    Merge branch 'main' of https://github.com/hwangjungeun/SemiProject.git into main
    Conflicts:
    	SemiProject/WebContent/WEB-INF/header.jsp

--- SemiProject/src/mypage/controller/PointListAction.java
+++ SemiProject/src/mypage/controller/PointListAction.java
@@ -1,12 +1,13 @@
 package mypage.controller;
 
 
-import java.util.Map;
+import java.util.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import common.controller.AbstractController;
+
 import mypage.model.*;
 
 public class PointListAction extends AbstractController {
@@ -31,7 +32,7 @@ public class PointListAction extends AbstractController {
 		
 		String userid = request.getParameter("userid");
 		
-		InterPointDAO pdao = new PointDAO();
+		InterPointDAO_HJE pdao = new PointDAO_HJE();
 		
 		// 총 적립금
 		
@@ -48,6 +49,87 @@ public class PointListAction extends AbstractController {
 		
 		int point_unuse = point_map.get("point_unuse");
 		request.setAttribute("point_unuse", point_unuse);
+		
+		// 사용자가 보고싶어하는 페이지 숫자
+		String currentShowPageNo = request.getParameter("currentShowPageNo");
+		
+		if(currentShowPageNo == null) {
+			currentShowPageNo = "1";
+		}
+		
+		try {
+			
+			Integer.parseInt(currentShowPageNo);
+			
+		} catch (NumberFormatException e) {
+			currentShowPageNo = "1";
+		}
+		
+		Map<String,String> paraMap = new HashMap<>();
+		
+		paraMap.put("userid", userid );
+		paraMap.put("currentShowPageNo", currentShowPageNo);
+		
+		List<PointVO_HJE> pointList =  pdao.selectPagingPoint(paraMap);
+		
+		request.setAttribute("pointList", pointList);
+		
+		String pageBar = "";
+		
+		int blockSize = 3;
+		// blockSize 는 블럭(토막)당 보여지는 페이지 번호의 개수이다.
+		
+		int loop = 1;
+		// loop는 1부터 증가하여 1개 블럭을 이루는 페이지번호의 개수(지금은 10개)까지만 증가하는 용도이다. 
+		
+		// !! 아래는 pageNo를 구하는 공식이다.
+		int pageNo = ( (Integer.parseInt(currentShowPageNo) - 1)/blockSize ) * blockSize + 1;
+		// pageNo 는 페이지바에서 보여지는 첫번째 번호이다.
+
+//		System.out.println(currentShowPageNo);
+		
+		// 페이징 처리를 위한 포인트에 대한 총 페이지 알아오기
+		int totalPage = pdao.getTotalPage(userid);
+
+		// *** [맨처음][이전] 만들기 *** //
+		if(pageNo != 1) {
+			pageBar += "<li class='page-item'><a class='page-link' href='pointList.go?userid="+userid+"&currentShowPageNo=1'>&laquo;&laquo;</a></li>";
+			pageBar += "<li class='page-item'><a class='page-link' href='pointList.go?userid="+userid+"&currentShowPageNo="+(pageNo-1)+"'>&laquo;</a></li>";
+		}
+		
+		
+		// *** pagenation 만들기 *** //
+		while( !(loop > blockSize || pageNo > totalPage) ) {
+			// 한 블럭(3)당 보여지는 페이지 번호의 개수보다 작을때,
+			// 메소드 getTotalPage을 통해 알아온 총 페이지 수보다 현재보여지는 페이지 수가 작을 떄
+			
+			if(pageNo == Integer.parseInt(currentShowPageNo)) { // 
+				pageBar += "<li class='page-item active'><a class='page-link' href='#'>"+pageNo+"</a></li>";
+			}
+			else {
+				pageBar += "<li class='page-item'><a class='page-link' href='pointList.go?userid="+userid+"&currentShowPageNo="+pageNo+"'>"+pageNo+"</a></li>";
+			}
+			
+			loop++;	// 1 2 3
+			
+			pageNo ++; //  1  2  3  
+					   //  4  5  6 
+					   //  7  8  9 
+			
+		} // end of while
+		
+		
+		// *** [다음][마지막] 만들기 *** //
+		if( pageNo <= totalPage ) {
+			pageBar += "<li class='page-item'><a class='page-link' href='pointList.go?userid="+userid+"&currentShowPageNo="+pageNo+"'>&raquo;</a></li>";
+			pageBar += "<li class='page-item'><a class='page-link' href='pointList.go?userid="+userid+"&currentShowPageNo="+totalPage+"'>&raquo;&raquo;</a></li>";
+		}
+		
+		
+		request.setAttribute("pageBar", pageBar);
+		request.setAttribute("userid", userid);
+		
+		/// *** === 페이지바 만들기 끝 === *** ///
 		
 		super.setRedirect(false);
 		super.setViewPage("/WEB-INF/mypage/pointList.jsp");
